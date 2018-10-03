8d237ac960c35907a9f31fc2a27a4a7e6bf5010c
github username <github email address>
2018-10-03 10:44:15 -0400

    resolved git issues

--- src/adapters/Adapter.js
+++ src/adapters/Adapter.js
@@ -18,10 +18,6 @@ export default class Adapter {
 			headers: {"Content-Type": "application/json"},
 			body: JSON.stringify({ user_id: userId })
 		})
-		.then(res => {
-			console.log(res)
-			return res
-		})
 		.then(res => res.json())
 	}
 	
